2b019cc24db9c7f5b00a2333c578b274bbb6cc9d
lukzgs <lucgs88@gmail.com>
2020-11-17 16:34:46 -0300

    cadastro, login, home

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,10 +2,17 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { PesquisaPalavraComponent } from './pesquisa-palavra/pesquisa-palavra.component';
 import { CriarPesquisaComponent } from './criar-pesquisa/criar-pesquisa.component';
+import { HomeComponent } from './home/home.component';
+import { LoginComponent } from './login/login.component';
+import { CadastroComponent } from './cadastro/cadastro.component';
 
 const routes: Routes = [
+  { path: '', redirectTo: '/home', pathMatch: 'full' },
   { path: 'pesquisa-palavra', component: PesquisaPalavraComponent},
-  { path: 'criar-pesquisa', component: CriarPesquisaComponent }
+  { path: 'criar-pesquisa', component: CriarPesquisaComponent },
+  { path: 'home', component: HomeComponent },
+  { path: 'login', component: LoginComponent },
+  { path: 'cadastro', component: CadastroComponent }
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,14 +5,20 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { PesquisaPalavraComponent } from './pesquisa-palavra/pesquisa-palavra.component';
 import { CriarPesquisaComponent } from './criar-pesquisa/criar-pesquisa.component';
+import { LoginComponent } from './login/login.component';
+import { HomeComponent } from './home/home.component';
+import { CadastroComponent } from './cadastro/cadastro.component';
 
-import { HttpClientModule } from '@angular/common/http'
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [
     AppComponent,
     PesquisaPalavraComponent,
-    CriarPesquisaComponent
+    CriarPesquisaComponent,
+    LoginComponent,
+    HomeComponent,
+    CadastroComponent
   ],
   imports: [
     BrowserModule,
